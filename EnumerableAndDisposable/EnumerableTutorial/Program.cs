19bcd6654c1fa5092116932cdc44074088c50cc3
Dmytro Korol <w70535@student.wsiz.edu.pl>
2025-04-15 20:06:30 +0200

    Tutorial Complited

--- EnumerableAndDisposable/EnumerableTutorial/Program.cs
+++ EnumerableAndDisposable/EnumerableTutorial/Program.cs
@@ -1,6 +1,11 @@
-﻿//Your code here (above the PersonName class)
+﻿using System.Collections;
+//Your code here (above the PersonName class)
 
-public class PersonName
+var rock = new PersonName("Dwayne", "Johnson", "Rock");
+
+Console.WriteLine(rock.Skip(1).First());
+
+public class PersonName : IEnumerable<string>
 {
     public string FirstName { get; }
     public string MiddleName { get; }
@@ -12,4 +17,40 @@ public class PersonName
         MiddleName = middleName;
         LastName = lastName;
     }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    public IEnumerator<string> GetEnumerator()
+    {
+        yield return FirstName;
+
+        if (MiddleName != null)
+            yield return MiddleName;
+
+        yield return LastName;
+
+    }
+
+    /*public class NameEnumerator : IEnumerator<string>
+    {
+        private readonly PersonName _person;
+        private int _currentIndex = -1;
+        public NameEnumerator(PersonName person) => _person = person;
+        public bool MoveNext()
+        {
+            _currentIndex++;
+            return _person.MiddleName != null && _currentIndex < 3 || _person.MiddleName == null && _currentIndex < 2;
+        }
+        public void Reset() => _currentIndex = -1;
+        public string Current
+        {
+            get 
+            {
+                if (_currentIndex == 0) return _person.FirstName;
+                if (_currentIndex == 1 && _person.MiddleName != null) return _person.MiddleName;
+                return _person.LastName;
+            }
+        }
+        object IEnumerator.Current => Current;
+        public void Dispose() { }
+    }*/
 }
--- EnumerableAndDisposable/FileBrowser/FileBrowserForm.cs
+++ EnumerableAndDisposable/FileBrowser/FileBrowserForm.cs
@@ -2,26 +2,60 @@ namespace FileBrowser;
 
 public partial class FileBrowserForm : Form
 {
+
+    private FilePreviewer _filePreviewer;
+
     public FileBrowserForm()
     {
         InitializeComponent();
+        nextButton.Enabled = false;
+        closeButton.Enabled = false;
     }
 
     private void openButton_Click(object sender, EventArgs e)
     {
         if (openFileDialog.ShowDialog() == DialogResult.OK)
         {
-
+            _filePreviewer = new FilePreviewer(openFileDialog.FileName);
+            resultTextBox.Text = _filePreviewer.GetNextLine();
+            openButton.Enabled = false;
+            nextButton.Enabled = true;
+            closeButton.Enabled = true;
         }
     }
 
     private void nextButton_Click(object sender, EventArgs e)
     {
-
+        resultTextBox.Text = _filePreviewer.GetNextLine();
     }
 
     private void closeButton_Click(object sender, EventArgs e)
     {
+        _filePreviewer.Dispose();
+        openButton.Enabled = true;
+        nextButton.Enabled = false;
+        resultTextBox.Clear();
+    }
+}
 
+public class FilePreviewer : IDisposable
+{
+    private readonly StreamReader _reader;
+
+    public FilePreviewer(string path) => _reader = new StreamReader(path);
+    
+    public string GetNextLine()
+    {
+        if (_reader.EndOfStream)
+        {
+            return "-- END OF FILE --";
+        }
+
+        return _reader.ReadLine();
+    }
+
+    public void Dispose()
+    {
+        _reader.Dispose();
     }
 }
